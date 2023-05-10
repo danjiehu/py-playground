403473bc4783f045f9722baa81af1ade69f33ee7
danjiehu <danjiehu@outlook.com>
2023-05-10 14:18:26 +0100

    start of nn demo

---
+++ nn-from-scratch.py
@@ -0,0 +1,2 @@
+# Neural Network from Scratch without ML libraries | 100 lines of Python code
+# https://www.youtube.com/watch?v=e-kIv_ht1XM
--- prog-01.py
+++ prog-01.py
@@ -1,3 +1,6 @@
 print("Hello, World!")
 name = input("What's your name? ")
 print("Hello {}!\nWelcome to Dataquest!".format(name))
+
+# code convention
+# https://fireship.io/lessons/code-this-not-that-python-edition/
